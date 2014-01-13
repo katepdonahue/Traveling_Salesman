f71f4260d01953be1789057eeadd5b7187cdbfb5
Kate <katepdonahue@gmail.com>
2014-01-13 17:03:39 -0500

    Create a trip and give it start and end points

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,6 +2,8 @@ class TripsController < ApplicationController
 
   def index
     @trip = Trip.new
+    @start = Waypoint.create(:name =>, :latitude =>, :longitude =>, :trip_id => @trip.id)
+    @end = Waypoint.create(:name =>, :latitude =>, :longitude =>, :trip_id => @trip.id)
   end
 
 end
