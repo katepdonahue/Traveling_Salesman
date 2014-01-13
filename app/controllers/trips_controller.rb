3ee246231712b3d99a56e029ee135bd851f3bf73
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-01-13 17:45:26 -0500

    add start marker

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,7 @@ class TripsController < ApplicationController
 
   def index
     @trip = Trip.new
-    @start = Waypoint.create(:name => "Start", :latitude => 40.697327, :longitude => 73.986135)
+    @start = Waypoint.create(:name => "Start", :latitude => 40.697327, :longitude => -73.986135)
     @end = Waypoint.create(:name =>"End", :latitude => 0, :longitude => 0)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TravelingSalesman::Application.routes.draw do
-  get '/test' => 'trips#index'
+  get '/trips' => 'trips#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
