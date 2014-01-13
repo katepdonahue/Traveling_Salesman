8a9e8860ce21a260cdceb5523ef6a3b6cfdf1c25
Kate <katepdonahue@gmail.com>
2014-01-13 15:22:40 -0500

    Start with one view rails app with a google maps api example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelingSalesman::Application.config.secret_token = 'c192e7e7ea60d63deb85068d2f10319eb68dbd2ad33957744062c096c9764ac3486d6e662ee2b026b01d89dd1909ae9eb724c6106db1b41339c813134e9bcfb6'
