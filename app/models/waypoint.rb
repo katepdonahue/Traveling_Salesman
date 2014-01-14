098cb03bb98fb97a7229957e8f98113cf89b0cb6
Kate <katepdonahue@gmail.com>
2014-01-14 13:57:04 -0500

    Add some stuff to the model to try to get gmaps to work

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -3,9 +3,10 @@ class Waypoint < ActiveRecord::Base
   has_many :trip_waypoints
   has_many :trips, :through => :trip_waypoints
 
+  include Gmaps4rails::ActsAsGmappable
   geocoded_by :address
   after_validation :geocode
-  acts_as_gmappable
+  acts_as_gmappable :process_geocoding => false
 
   def gmaps4rails_address
     address
