231dfcae282866117db4278dbb284cc7134a67d0
Kate <katepdonahue@gmail.com>
2014-01-14 18:56:00 -0500

    Make the form work

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,10 +2,23 @@ class TripsController < ApplicationController
 
   def index
     @trip = Trip.new
-    @trip.waypoints.build(:name => "Start", :latitude => 40.697327, :longitude => -73.986135)
+    # @trip.waypoints.build(:name => "Start", :latitude => 40.697327, :longitude => -73.986135)
     @trip.waypoints.build(:name =>"End", :latitude => 40.748444, :longitude => -73.985658)
     @trip.save
-    @waypoint = Waypoint.new
+    @hash = Gmaps4rails.build_markers(@trip.waypoints) do |waypoint, marker|
+      marker.lat waypoint.latitude
+      marker.lng waypoint.longitude
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    @trip.waypoints.build(:name => params[:name], :latitude => params[:latitude].to_i, :longitude => params[:longitude].to_i)
+    @trip.save
     @hash = Gmaps4rails.build_markers(@trip.waypoints) do |waypoint, marker|
       marker.lat waypoint.latitude
       marker.lng waypoint.longitude
@@ -13,7 +26,7 @@ class TripsController < ApplicationController
   end
 
   def show
-
+    @trip = Trip.find(params[:id])
   end
 
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,5 @@
 class Trip < ActiveRecord::Base
-	attr_accessible :name
+	attr_accessible :name, :waypoints_attributes
   has_many :trip_waypoints
   has_many :waypoints, :through => :trip_waypoints
-
 end
