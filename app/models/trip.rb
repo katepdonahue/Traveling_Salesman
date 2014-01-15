2d68e07e67e0dbde71d2eff75b330d64f9e49a73
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-01-14 19:40:07 -0500

    add nested attributes to model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,7 @@
 class Trip < ActiveRecord::Base
 	attr_accessible :name, :waypoints_attributes
-  has_many :trip_waypoints
+  has_many :trip_waypoints, inverse_of: :waypoint
   has_many :waypoints, :through => :trip_waypoints
+
+  accepts_nested_attributes_for :trip_waypoints
 end
--- app/models/trip_waypoint.rb
+++ app/models/trip_waypoint.rb
@@ -2,4 +2,9 @@ class TripWaypoint < ActiveRecord::Base
   attr_accessible :trip_id
   belongs_to :trip
   belongs_to :waypoint
+
+  validate_presence_of :trip
+  validate_presence_of :waypoint
+
+  accepts_nested_attributes_for :waypoint
 end
