b6cab8807f85d34a36793c0e2d314bc6d5d23b5e
Kate <katepdonahue@gmail.com>
2014-01-16 13:43:36 -0500

    Change form to use geocode

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -1,5 +1,7 @@
 class Waypoint < ActiveRecord::Base
   attr_accessible :name, :latitude, :longitude, :address
+  geocoded_by :address
+  after_validation :geocode
   has_many :trip_waypoints
   has_many :trips, :through => :trip_waypoints
 
