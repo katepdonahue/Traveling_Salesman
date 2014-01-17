d8a24f8199c256a89650a4b0652bf11da3e9ebe8
Kate <katepdonahue@gmail.com>
2014-01-17 13:31:25 -0500

    First try at ajax

---
+++ app/assets/javascripts/ajax.js
@@ -0,0 +1,25 @@
+function loadJSON()
+{
+  var post_file = "/trips"
+  var get_file = "/trips/:id"
+  if (window.XMLHttpRequest) {// IE7+, Firefox, Chrome, Opera, Safari
+    http_request = new XMLHttpRequest();
+  }
+  else { // IE6, IE5
+    http_request = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  http_request.onreadystatechange = function() {
+    if (http_request.readyState == 4 && http_request.status == 200) {
+      document.getElementById("map-canvas").innerHTML=http_request.responseText;
+    }
+  }
+  http_request.open("POST", post_file, true);
+  http_request.send();
+  // http_request.open("GET", get_file, true);
+  // http_request.send();
+}
+
+
+
+// http://www.tutorialspoint.com/json/json_ajax_example.htm
+// http://www.w3schools.com/ajax/tryit.asp?filename=tryajax_first
