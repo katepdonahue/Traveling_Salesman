0709c8e787374e0cfde77cfb4258bba2d10d5559
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-01-20 18:00:35 -0500

    add methods to trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,4 @@
-require "open-uri"
+require "httparty"
 
 class Trip < ActiveRecord::Base
 	attr_accessible :name, :waypoints_attributes
@@ -15,8 +15,17 @@ class Trip < ActiveRecord::Base
   def time(i, j)
     trip_origin = self.waypoints[i].address.gsub(" ", "%20")
     trip_destination = self.waypoints[j].address.gsub(" ", "%20")
-    file = open("https://maps.googleapis.com/maps/api/directions/json?origin=#{trip_origin}&destination=#{trip_destination}&sensor=false")
-    duration = file["routes"]["legs"]["duration"]["text"]
+    file = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{trip_origin}&destination=#{trip_destination}&TravelMode=TRANSIT&sensor=false&departure_time=1343641500&mode=transit")
+    #Time.to_i gives Unix time
+    debugger
+    file["routes"][0]["legs"][0]["duration"]["text"]
+  end
+
+  def to_mins(duration)
+    match = /(\d*)\shours?\s(\d*)\smins?/.match(duration)
+    hour = match[1]
+    mins = match[2]
+    hour.to_i * 60 + mins.to_i
   end
 
 end
