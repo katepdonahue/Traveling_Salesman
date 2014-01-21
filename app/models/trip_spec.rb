37d8ba3d0e2ca2057a25ad597fd87e134d8ef4a3
Kate <katepdonahue@gmail.com>
2014-01-21 10:53:42 -0500

    Write tests for to_mins method

--- app/models/trip_spec.rb
+++ app/models/trip_spec.rb
@@ -1,13 +1,19 @@
 require './trip.rb'
 
 describe Trip do
+  trip = Trip.new
+  trip.waypoints.build(:name => "first", :address => "25 chapel st. Brooklyn, Ny 11201")
+  trip.waypoints.build(:name => "second", :address => "Empire State Building")
   
-  describe "#time" do
-    
-  end
-
   describe "#to_mins" do
-
+    it "should work for singular hours and mins" do
+      # stub time(0, 1) to return "1 hour 1 min"
+      expect(trip.to_mins(0, 1)).to eq(61)
+    end
+    it "should work for plural hours and mins" do
+      # stub time(0, 1) to return "2 hours 3 mins"
+      expect(trip.to_mins(0, 1)).to eq(123)
+    end
   end
 
   describe "#ways" do
