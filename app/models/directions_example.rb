4e524eb3bf24394e8cf7c77541935e90d0e2c648
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-01-23 18:00:20 -0500

    add directions example file

---
+++ app/models/directions_example.rb
@@ -0,0 +1,279 @@
+{
+  "routes": [
+    {
+      "bounds": {
+        "northeast": {
+          "lat": 40.749719,
+          "lng": -73.9845368
+        },
+        "southwest": {
+          "lat": 40.6972496,
+          "lng": -74.002531
+        }
+      },
+      "copyrights": "Map data \u00a92014 Google",
+      "legs": [
+        {
+          "arrival_time": {
+            "text": "5:18pm",
+            "time_zone": "America\/New_York",
+            "value": 1390515528
+          },
+          "departure_time": {
+            "text": "4:55pm",
+            "time_zone": "America\/New_York",
+            "value": 1390514115
+          },
+          "distance": {
+            "text": "4.6 mi",
+            "value": 7469
+          },
+          "duration": {
+            "text": "24 mins",
+            "value": 1413
+          },
+          "end_address": "Empire State Building, 350 5th Avenue, New York, NY 10118, USA",
+          "end_location": {
+            "lat": 40.7480183,
+            "lng": -73.9848535
+          },
+          "start_address": "25 Chapel Street, Brooklyn, NY 11201, USA",
+          "start_location": {
+            "lat": 40.6972496,
+            "lng": -73.9861397
+          },
+          "steps": [
+            {
+              "distance": {
+                "text": "0.2 mi",
+                "value": 330
+              },
+              "duration": {
+                "text": "4 mins",
+                "value": 255
+              },
+              "end_location": {
+                "lat": 40.699743,
+                "lng": -73.986885
+              },
+              "html_instructions": "Walk to York St",
+              "polyline": {
+                "points": "ytkwFjlqbMErDoAEcDK_@AoAKa@AaACc@D"
+              },
+              "start_location": {
+                "lat": 40.6972496,
+                "lng": -73.9861397
+              },
+              "steps": [
+                {
+                  "distance": {
+                    "text": "249 ft",
+                    "value": 76
+                  },
+                  "duration": {
+                    "text": "1 min",
+                    "value": 60
+                  },
+                  "end_location": {
+                    "lat": 40.69728,
+                    "lng": -73.98704
+                  },
+                  "html_instructions": "Head <b>west<\/b> on <b>Chapel St<\/b> toward <b>Jay St<\/b>",
+                  "polyline": {
+                    "points": "ytkwFjlqbMErD"
+                  },
+                  "start_location": {
+                    "lat": 40.6972496,
+                    "lng": -73.9861397
+                  },
+                  "travel_mode": "WALKING"
+                },
+                {
+                  "distance": {
+                    "text": "0.2 mi",
+                    "value": 254
+                  },
+                  "duration": {
+                    "text": "3 mins",
+                    "value": 195
+                  },
+                  "end_location": {
+                    "lat": 40.699743,
+                    "lng": -73.986885
+                  },
+                  "html_instructions": "Turn <b>right<\/b> onto <b>Jay St<\/b><div style=\"font-size:0.9em\">Destination will be on the left<\/div>",
+                  "maneuver": "turn-right",
+                  "polyline": {
+                    "points": "_ukwF~qqbMoAEcDK_@AoAKa@AaACc@D"
+                  },
+                  "start_location": {
+                    "lat": 40.69728,
+                    "lng": -73.98704
+                  },
+                  "travel_mode": "WALKING"
+                }
+              ],
+              "travel_mode": "WALKING"
+            },
+            {
+              "distance": {
+                "text": "4.2 mi",
+                "value": 6792
+              },
+              "duration": {
+                "text": "15 mins",
+                "value": 900
+              },
+              "end_location": {
+                "lat": 40.749719,
+                "lng": -73.987823
+              },
+              "html_instructions": "Subway towards Jamaica - 179 St",
+              "polyline": {
+                "points": "kdlwF~pqbMgZy@o@Aq@@q@@o@Fo@Fo@Jo@Lo@Po@Ro@Tm@Vo@Zm@\\m@`@m@b@yE`D[T_@T_@Ra@Ra@Pa@Pc@Nc@Ne@Ne@Le@Ji@Jg@Ji@Hi@HgGf@K@M@M@M@K@K@K?K@K@K?K@I?I@I?K?G@G?I?G@G?G?G?G?I?G?G?GAE?G?GAKAKAKAKAICKAMCKCKEKCKEMEKEKEKGaT{ImMkFc@Oa@Ka@G]C]?[BYHYJUPUTUXQ\\O`@Od@Mj@yBrK{ChO_FxU[vAETGTGRGTGRGRGRGRGRGPIRGPGPGPINsJtVKVKTMPMNOLOJOFQFSBS?U?UAUEWGYKsHgDCAECEACAECEACCEACCECECCAECECECyE_DEACC_EkCu]iUa\\eTwi@g^"
+              },
+              "start_location": {
+                "lat": 40.699743,
+                "lng": -73.986885
+              },
+              "transit_details": {
+                "arrival_stop": {
+                  "location": {
+                    "lat": 40.749719,
+                    "lng": -73.987823
+                  },
+                  "name": "34 St - Herald Sq"
+                },
+                "arrival_time": {
+                  "text": "5:14pm",
+                  "time_zone": "America\/New_York",
+                  "value": 1390515270
+                },
+                "departure_stop": {
+                  "location": {
+                    "lat": 40.699743,
+                    "lng": -73.986885
+                  },
+                  "name": "York St"
+                },
+                "departure_time": {
+                  "text": "4:59pm",
+                  "time_zone": "America\/New_York",
+                  "value": 1390514370
+                },
+                "headsign": "Jamaica - 179 St",
+                "line": {
+                  "agencies": [
+                    {
+                      "name": "MTA New York City Transit",
+                      "phone": "(718) 330-1234",
+                      "url": "http:\/\/www.mta.info\/"
+                    }
+                  ],
+                  "color": "#f25e18",
+                  "icon": "\/\/maps.gstatic.com\/mapfiles\/transit\/iw\/6\/us-ny-mta\/F.png",
+                  "name": "Queens Blvd Express\/ 6 Av Local",
+                  "short_name": "F",
+                  "url": "http:\/\/www.mta.info\/nyct\/service\/pdf\/tfcur.pdf",
+                  "vehicle": {
+                    "icon": "\/\/maps.gstatic.com\/mapfiles\/transit\/iw\/6\/metro.png",
+                    "name": "Subway",
+                    "type": "SUBWAY"
+                  }
+                },
+                "num_stops": 8
+              },
+              "travel_mode": "TRANSIT"
+            },
+            {
+              "distance": {
+                "text": "0.2 mi",
+                "value": 347
+              },
+              "duration": {
+                "text": "4 mins",
+                "value": 258
+              },
+              "end_location": {
+                "lat": 40.7480183,
+                "lng": -73.9848535
+              },
+              "html_instructions": "Walk to Empire State Building, 350 5th Avenue, New York, NY 10118, USA",
+              "polyline": {
+                "points": "w|uwFzvqbM@m@~FaRpA|@"
+              },
+              "start_location": {
+                "lat": 40.749719,
+                "lng": -73.987823
+              },
+              "steps": [
+                {
+                  "distance": {
+                    "text": "0.2 mi",
+                    "value": 294
+                  },
+                  "duration": {
+                    "text": "4 mins",
+                    "value": 217
+                  },
+                  "end_location": {
+                    "lat": 40.7484326,
+                    "lng": -73.9845368
+                  },
+                  "html_instructions": "Head <b>southeast<\/b> on <b>W 34th St<\/b> toward <b>5th Ave<\/b>",
+                  "polyline": {
+                    "points": "w|uwFzvqbM@m@~FaR"
+                  },
+                  "start_location": {
+                    "lat": 40.749719,
+                    "lng": -73.987823
+                  },
+                  "travel_mode": "WALKING"
+                },
+                {
+                  "distance": {
+                    "text": "174 ft",
+                    "value": 53
+                  },
+                  "duration": {
+                    "text": "1 min",
+                    "value": 41
+                  },
+                  "end_location": {
+                    "lat": 40.7480183,
+                    "lng": -73.9848535
+                  },
+                  "html_instructions": "Turn <b>right<\/b> onto <b>5th Ave<\/b><div style=\"font-size:0.9em\">Destination will be on the right<\/div>",
+                  "maneuver": "turn-right",
+                  "polyline": {
+                    "points": "utuwFjbqbMpA|@"
+                  },
+                  "start_location": {
+                    "lat": 40.7484326,
+                    "lng": -73.9845368
+                  },
+                  "travel_mode": "WALKING"
+                }
+              ],
+              "travel_mode": "WALKING"
+            }
+          ],
+          "via_waypoint": [
+            
+          ]
+        }
+      ],
+      "overview_polyline": {
+        "points": "ytkwFjlqbMErDoAEcEMqBMaACc@Dw[{@cBB_BN_BX_Bd@}Al@}Ax@{AdAuFvD_Ah@cAd@eA`@uCx@eDj@aJr@{CJcAEm@Iq@Oq@Ugc@uQeA[_AKy@Bs@Tk@f@g@v@_@fAgC~L{Jbf@a@lB_@rAaAxCqKnXYf@]\\_@Re@Ji@?k@Gq@SwHiDUKi@YghAit@wi@g^@m@~FaRpA|@"
+      },
+      "summary": "",
+      "warnings": [
+        "Walking directions are in beta.    Use caution \u2013 This route may be missing sidewalks or pedestrian paths."
+      ],
+      "waypoint_order": [
+        
+      ]
+    }
+  ],
+  "status": "OK"
+}
