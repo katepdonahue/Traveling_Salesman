bd1b405f7ab3047e87e02a5aee6c4d8ac478e5ba
Kate <katepdonahue@gmail.com>
2014-01-24 12:05:46 -0500

    Steps table is just like legs table

--- db/migrate/20140124162921_steps.rb
+++ db/migrate/20140124162921_steps.rb
@@ -1,4 +1,24 @@
 class Steps < ActiveRecord::Migration
   def change
+    create_table :legs do |t|
+      t.string :distance
+      t.string :duration
+      t.string :html_instructions
+      t.string :travel_mode
+      t.string :travel_mode
+      t.string :polyline
+      # everything below this is only for transit legs
+      t.string :arrival_time
+      t.float :arrival_stop_lat
+      t.float :arrival_stop_lng
+      t.float :departure_time
+      t.float :departure_stop_lat
+      t.float :departure_stop_lng
+      t.string :headsign
+      t.string :color
+      t.string :icon
+      t.string :long_name
+      t.string :short_name
+    end
   end
 end
