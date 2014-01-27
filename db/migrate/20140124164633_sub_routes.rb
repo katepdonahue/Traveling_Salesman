92b0bb0e9ae709bf1d9cbd8f46822dfbd0bcc44a
Kate <katepdonahue@gmail.com>
2014-01-27 11:26:12 -0500

    Should be waypoint id

--- db/migrate/20140124164633_sub_routes.rb
+++ db/migrate/20140124164633_sub_routes.rb
@@ -1,8 +1,8 @@
 class SubRoutes < ActiveRecord::Migration
   def change
     create_table :routes do |t|
-      t.string :origin_waypoint
-      t.string :destination_waypoint
+      t.string :origin_waypoint_id
+      t.string :destination_waypoint_id
       t.integer :departure_time
       t.string :json
     end
