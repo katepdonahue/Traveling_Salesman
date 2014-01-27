18b93a283c0416c087a7fa2f4c3d1bd5276902d8
Kate <katepdonahue@gmail.com>
2014-01-27 11:28:02 -0500

    A trip has many sub_routes

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,7 @@ class Trip < ActiveRecord::Base
 	attr_accessible :name, :waypoints_attributes
   has_many :trip_waypoints, inverse_of: :waypoint
   has_many :waypoints, :through => :trip_waypoints
+  has_many :sub_routes
 
   accepts_nested_attributes_for :trip_waypoints
   accepts_nested_attributes_for :waypoints
