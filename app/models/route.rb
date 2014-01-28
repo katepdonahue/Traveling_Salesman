f25d169a12a64a5c6b4cd1ca584cc1bee88773eb
Kate <katepdonahue@gmail.com>
2014-01-28 18:45:02 -0500

    Transfer total_time method to route class, rename and repurpose

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,15 @@ class Route < ActiveRecord::Base
   attr_accessible :total_time, :departure_time, :trip_id, :sub_routes, :sub_routes_attributes, :best
   has_many :sub_routes
   belongs_to :trip
+
+  def duration
+    total = 0
+    start_time = self.departure_time
+    self.sub_routes.each do |sub_route|
+      total += sub_route.to_mins
+      start_time += total * 60 
+    end
+    total
+  end
+
 end
