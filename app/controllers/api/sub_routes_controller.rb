55ed75ca405a86ec8ec57e0663da34fc1a325cb1
Kate <katepdonahue@gmail.com>
2014-01-29 17:42:34 -0500

    Add show action for sub_routes controller to get access to google_results

--- app/controllers/api/sub_routes_controller.rb
+++ app/controllers/api/sub_routes_controller.rb
@@ -1,5 +1,12 @@
 module Api
+
   class SubRoutesController < ApplicationController
+    respond_to :json
+
+    def show
+      respond_with SubRoute.find(params[:id]).google_results
+    end
 
   end
+
 end
