14e35081e0bce3f9ebb3b3d60142071efcdf9971
Jonathan Brown <jonathanbrown.a@gmail.com>
2014-01-29 16:09:42 -0500

    fix addmarker thanks to nikki

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -14,22 +14,33 @@ function initialize() {
   // directionsDisplay.setMap(map);
 }
 
-function addMarker() {
-  var addresses = document.getElementsByClassName("waypoints");
-  for (var i=0; i<addresses.length; i++) {
-    address = addresses[i].value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-  };
+function addMarker(address) {
+  // var addresses = document.getElementsByClassName("waypoints");
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+  // for (var i=0; i<addresses.length; i++) {
+  //   address = addresses[i].value;
+  //   geocoder.geocode( { 'address': address}, function(results, status) {
+  //     if (status == google.maps.GeocoderStatus.OK) {
+  //       map.setCenter(results[0].geometry.location);
+  //       var marker = new google.maps.Marker({
+  //           map: map,
+  //           position: results[0].geometry.location
+  //       });
+  //     } else {
+  //       alert("Geocode was not successful for the following reason: " + status);
+  //     }
+  //   });
+  // };
 }
 
 function calcRoute() {
