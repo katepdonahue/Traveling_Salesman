751072fe98524a6465b117e2e555ea17bec80c9d
Kate <katepdonahue@gmail.com>
2014-02-03 11:07:48 -0500

    Comment out actions that I don't think we are using

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,21 +1,21 @@
 class TripsController < ApplicationController
 
-  def index
-    @trip = Trip.new
-  end
+  # def index
+  #   @trip = Trip.new
+  # end
 
   def new
     @trip = Trip.new
     @trip.waypoints.build
   end
 
-  def create
-    @trip = Trip.new(params[:trip])
-    @trip.save
-    @trip.request
-    redirect_to "/trips/#{@trip.id}" #with ajax don't do this cuz will send get request to show right after
-    # render :nothing => true 
-  end
+  # def create
+  #   @trip = Trip.new(params[:trip])
+  #   @trip.save
+  #   @trip.request
+  #   redirect_to "/trips/#{@trip.id}" #with ajax don't do this cuz will send get request to show right after
+  #   # render :nothing => true 
+  # end
 
   def show
     @trip = Trip.find(params[:id])
@@ -29,10 +29,10 @@ class TripsController < ApplicationController
 
 
   def calculate
-    trip = Trip.find(165)
+    trip = Trip.new
     subroute = trip.routes.first.sub_routes.first
     @whatever = subroute.format_sub_r_request.to_json
-    render :json => @whatever
+    render "/trips/#{@trip.id}"
   end
 
 end
